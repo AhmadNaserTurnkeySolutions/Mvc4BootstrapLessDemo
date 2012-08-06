d84252417eae06ddf6b34476630e65b1795e66ec
Michael <mike@michaeljbaird.com>
2012-08-06 14:12:09 -0500

    Initial Commit

---
+++ App_Start/AppActivator.cs
@@ -0,0 +1,43 @@
+﻿[assembly: WebActivator.PreApplicationStartMethod(typeof(Mvc4BootstrapLessDemo.AppActivator), "PreStart")]
+[assembly: WebActivator.PostApplicationStartMethod(typeof(Mvc4BootstrapLessDemo.AppActivator), "PostStart")]
+[assembly: WebActivator.ApplicationShutdownMethodAttribute(typeof(Mvc4BootstrapLessDemo.AppActivator), "Stop")]
+namespace Mvc4BootstrapLessDemo
+{
+  using System;
+  using System.Collections.Generic;
+  using System.Linq;
+  using System.Web;
+  using System.Web.Mvc;
+  using System.Web.Optimization;
+  using System.Web.Routing;
+
+  public static class AppActivator
+  {
+    public static void PreStart()
+    {
+
+    }
+
+    public static void PostStart()
+    {
+      AppPostStart();
+    }
+
+    public static void Stop()
+    {
+
+    }
+
+    private static void AppPostStart()
+    {
+      ViewEngines.Engines.Clear();
+      ViewEngines.Engines.Add(new RazorViewEngine());
+
+      AreaRegistration.RegisterAllAreas();
+
+      RouteConfig.RegisterRoutes(RouteTable.Routes);
+      FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+      BundleConfig.RegisterBundles(BundleTable.Bundles);
+    }
+  }
+}
---
+++ App_Start/BundleConfig.cs
@@ -0,0 +1,47 @@
+﻿namespace Mvc4BootstrapLessDemo
+{
+  using System.Web.Optimization;
+
+  public class BundleConfig
+  {
+    public static void RegisterBundles(BundleCollection bundles)
+    {
+      BundleTable.EnableOptimizations = true;
+
+      // jQuery UI
+      var jqueryUICss = new StyleBundle("~/jqueryui.css").Include(
+        "~/Content/themes/base/jquery.ui.core.css",
+        "~/Content/themes/base/jquery.ui.resizable.css",
+        "~/Content/themes/base/jquery.ui.selectable.css",
+        "~/Content/themes/base/jquery.ui.accordion.css",
+        "~/Content/themes/base/jquery.ui.autocomplete.css",
+        "~/Content/themes/base/jquery.ui.button.css",
+        "~/Content/themes/base/jquery.ui.dialog.css",
+        "~/Content/themes/base/jquery.ui.slider.css",
+        "~/Content/themes/base/jquery.ui.tabs.css",
+        "~/Content/themes/base/jquery.ui.datepicker.css",
+        "~/Content/themes/base/jquery.ui.progressbar.css",
+        "~/Content/themes/base/jquery.ui.theme.css");
+      jqueryUICss.Transforms.Add(new CssMinify());
+      bundles.Add(jqueryUICss);
+
+      // Default
+      var css = new StyleBundle("~/default.css").Include(
+        "~/Content/less/bootstrap.less",
+        "~/Content/less/responsive.less",
+        "~/Content/prettify.css",
+        "~/Content/site.css");
+      css.Transforms.Add(new LessMinify());
+      bundles.Add(css);
+
+      var javascript = new ScriptBundle("~/default.js").Include(
+        "~/Scripts/jquery-1.*",
+        "~/Scripts/jquery-ui*",
+        "~/Scripts/prettify.js",
+        "~/Scripts/bootstrap.min.js",
+        "~/Scripts/Main.js");
+      javascript.Transforms.Add(new JsMinify());
+      bundles.Add(javascript);
+    }
+  }
+}
